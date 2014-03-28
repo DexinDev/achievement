96ab395b05e904729b1d516cad671cfa75d724b5
Danil Golovizin <dabil@rutorika.ru>
2014-03-28 16:25:08 +0400

    Логер и база

---
+++ db/users.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 28 2014 г., 12:23
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.4.26
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `achievments`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`) VALUES
+(1, 'Максимильян Буян'),
+(2, 'Дима Гром'),
+(3, 'Леха Ли'),
+(4, 'Миша Топор');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- public/index.php
+++ public/index.php
@@ -14,28 +14,42 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../log/development.log',
+));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'dbname'   => 'achievments',
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => null,
+    ),
+));
+
+
 $app->get('/', function() use ($app) {
+    $app['monolog']->addDebug('Log-log - monolog');
     return $app['twig']->render('layout.twig', array(
         'content' => 'Главная'
     ));
 });
 
-$users = array(
-    1 => array('name' => 'Maxim Buyanov'),
-    2 => array('name' => 'Alex Li'),
-    3 => array('name' => 'Yana Kidalova'),
-    4 => array('name' => 'Sofa Efimova'),
-);
-
 $app->get('/people', function() use ($app) {
     return $app['twig']->render('layout.twig', array('content' => 'Список людей'));
 });
 
-$app->get('/people/{id}', function($id) use ($users, $app)  {
+$app->get('/people/{id}', function($id) use ($app)  {
+    $sql = "SELECT count(id) FROM users WHERE id = ?";
+    $count = $app['db']->fetchAssoc($sql, array((int) $id));
+    if ($count['count(id)'] > 0) {
 
-    if (array_key_exists($id, $users)) {
+        $sql = "SELECT name FROM users WHERE id = ?";
+        $user = $app['db']->fetchAssoc($sql, array((int) $id));
+
         return $app['twig']->render('layout.twig', array(
-            'content' => $app['twig']->render('people/view.twig', array('user' => $users[$id]))
+            'content' => $app['twig']->render('people/view.twig', array('user' => $user))
         ));
     } else {
         $app->abort(404, 'Такого пользователя не существует');
